afcbad3d3d43af46ba8b42740c82dee2b797534d
kumarananda <coderanandakumar@gmail.com>
2022-02-07 03:42:50 +0600

    u

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 
 
+// const dd_item = document.querySelectorAll('.left-body-menu ul li a');
+// const dd = document.querySelectorAll('.main-contetn .left-body .left-body-menu ul li .dropdwon');
+
+
+
+const tabManu = document.querySelectorAll('.left-body-menu ul li a');
+const tab_pan_all = document.querySelectorAll('.c17_tabSec .aks_tab .tab-body .tab-pane')
+
+tabManu.forEach(item => {
+    item.addEventListener('click', function (e) {
+        e.preventDefault()
+        tabManu.forEach(item => {
+            item.classList.remove('active')
+        })
+        
+        item.classList.add('active');
+        const get_pan = document.querySelector(this.getAttribute('href'));
+        tab_pan_all.forEach(item => {
+            item.classList.remove('active')
+        })
+        get_pan.classList.add('active')
+        
+    })
+})
+
